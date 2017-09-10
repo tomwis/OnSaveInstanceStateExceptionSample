1046213b592eeabd68a96c85cccf48786d95ba53
Tomasz Wiśniewski <tomek_wisniewski@interia.eu>
2017-09-10 19:24:25 +0200

    First commit

--- OnSaveInstanceStateExceptionSample/OnSaveInstanceStateExceptionSample.Android/MainActivityWithQueue.cs
+++ OnSaveInstanceStateExceptionSample/OnSaveInstanceStateExceptionSample.Android/MainActivityWithQueue.cs
@@ -1,4 +1,4 @@
-﻿#if !DEBUG_CORRECT
+﻿#if DEBUG_CORRECT
 using System;
 
 using Android.App;
@@ -7,27 +7,31 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-
+using System.Collections.Generic;
 
 namespace OnSaveInstanceStateExceptionSample.Droid
 {
-    [Activity(Label = "OnSaveInstanceStateExceptionSample.Android", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
-    {
+	[Activity (Label = "OnSaveInstanceStateExceptionSample.Android", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivityWithQueue : Activity
+	{
         Switch _switch;
         TextView _instruction;
+        bool _instanceStateSaved;
+        List<Type> _fragmentsQueue;
 
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
+        protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+            _fragmentsQueue = new List<Type>();
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
             _switch = FindViewById<Switch>(Resource.Id.switch1);
             _switch.CheckedChange += Switch1_CheckedChange;
 
             _instruction = FindViewById<TextView>(Resource.Id.instruction);
-            _instruction.Text = "Now leave the app with home button (not back button) to crash it.";
+            _instruction.Text = "Now leave the app with home button (not back button) and open it again. It won't crash and fragment will load.";
         }
 
         private void Switch1_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
@@ -35,10 +39,10 @@ namespace OnSaveInstanceStateExceptionSample.Droid
             _instruction.Visibility = e.IsChecked ? ViewStates.Visible : ViewStates.Gone;
         }
 
-        void AddFragment()
+        void AddFragment(Type fragmentType)
         {
             System.Diagnostics.Debug.WriteLine("AddFragment");
-            var fragment = new MyFragment();
+            var fragment = (Fragment) Activator.CreateInstance(fragmentType);
             FragmentManager
                 .BeginTransaction()
                 .Replace(Resource.Id.fragment_container, fragment)
@@ -47,8 +51,22 @@ namespace OnSaveInstanceStateExceptionSample.Droid
 
         protected override void OnSaveInstanceState(Bundle outState)
         {
+            _instanceStateSaved = true;
             base.OnSaveInstanceState(outState);
             System.Diagnostics.Debug.WriteLine("OnSaveInstanceState");
+        }
+
+        protected override void OnPostResume()
+        {
+            base.OnPostResume();
+            System.Diagnostics.Debug.WriteLine("OnPostResume");
+            _instanceStateSaved = false;
+
+            for(int i = 0; i < _fragmentsQueue.Count;)
+            {
+                AddFragment(_fragmentsQueue[i]);
+                _fragmentsQueue.RemoveAt(0);
+            }
         }
 
         protected override void OnPause()
@@ -61,10 +79,18 @@ namespace OnSaveInstanceStateExceptionSample.Droid
         {
             base.OnStop();
             System.Diagnostics.Debug.WriteLine("OnStop");
-
+            
             if (_switch.Checked)
             {
-                AddFragment();
+                if (_instanceStateSaved)
+                {
+                    System.Diagnostics.Debug.WriteLine("Save fragment for later");
+                    _fragmentsQueue.Add(typeof(MyFragment));
+                }
+                else
+                {
+                    AddFragment(typeof(MyFragment));
+                }
             }
         }
     }
--- OnSaveInstanceStateExceptionSample/OnSaveInstanceStateExceptionSample.Android/Resources/Resource.designer.cs
+++ OnSaveInstanceStateExceptionSample/OnSaveInstanceStateExceptionSample.Android/Resources/Resource.designer.cs
@@ -60,8 +60,14 @@ namespace OnSaveInstanceStateExceptionSample.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int fragment_container = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int instruction = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int switch1 = 2131034112;
 			
 			static Id()
 			{
@@ -78,6 +84,9 @@ namespace OnSaveInstanceStateExceptionSample.Droid
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int my_fragment = 2130903041;
 			
 			static Layout()
 			{
